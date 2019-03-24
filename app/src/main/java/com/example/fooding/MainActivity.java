06a0bb90ff1f07a375820297b7bbc48d8c4b866e
giovannic96 <giovanni.cala@hotmail.it>
2019-03-24 19:43:20 +0100

    Fixed photo capture

--- app/src/main/java/com/example/fooding/MainActivity.java
+++ app/src/main/java/com/example/fooding/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.fooding;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -27,6 +29,10 @@ import java.util.Objects;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAM_REQ = 10;
+    private static final int GALLERY_REQ = 11;
+    private static final int STORAGE_PERMISSION_CODE = 100;
 
     private CircleImageView image_btn;
     private Uri file;
@@ -38,8 +44,24 @@ public class MainActivity extends AppCompatActivity {
 
         image_btn = findViewById(R.id.avatar);
         image_btn.setOnClickListener(e -> {
-            selectImage();
+            if(isStoragePermissionGranted()) {
+                selectImage();
+            }
         });
+    }
+
+    public boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission())
+                return true;
+            else
+                requestStoragePermission();
+        }
+        else {
+            Log.d("MAD-permission","Permission is granted");
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -65,32 +87,48 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
+    @SuppressLint("IntentReset")
     private void galleryIntent() {
-        Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, GALLERY_REQ);
     }
 
     private void cameraIntent() {
         Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        file = Uri.fromFile(getOutputMediaFile());
+
+        file = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", Objects.requireNonNull(getOutputMediaFile()));
         takePicture.putExtra(MediaStore.EXTRA_OUTPUT, file);
-        startActivityForResult(takePicture, 0);//zero can be replaced with any action code
+        takePicture.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivityForResult(takePicture, CAM_REQ);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
         switch (requestCode) {
-            case 0:
-                if (resultCode == RESULT_OK) {
+            case CAM_REQ:
+                if (imageReturnedIntent != null && resultCode == RESULT_OK) {
+                    /* Bitmap bm = (Bitmap)imageReturnedIntent.getExtras().get("data");
+                    image_btn.setImageBitmap(bm); */
                     image_btn.setImageURI(file);
                 }
-
                 break;
-            case 1:
-                if (resultCode == RESULT_OK) {
+            case GALLERY_REQ:
+                if (imageReturnedIntent != null && resultCode == RESULT_OK) {
+
                     Uri selectedImage = imageReturnedIntent.getData();
                     image_btn.setImageURI(selectedImage);
+
                 }
                 break;
         }
@@ -100,14 +138,44 @@ public class MainActivity extends AppCompatActivity {
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "CameraDemo");
 
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MAD-photo", "Oops! Failed create directory");
                 return null;
             }
         }
 
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +
-                "IMG_"+ timeStamp + ".jpg");
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +"IMG_"+ timeStamp + ".jpg");
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed to store images")
+                    .setPositiveButton("Ok", (dialog, which) -> ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE))
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case STORAGE_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
