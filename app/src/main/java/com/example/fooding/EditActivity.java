ea41393fe88e0554452eec7a71ffc2ec30cdb617
giovannic96 <giovanni.cala@hotmail.it>
2019-03-29 23:25:16 +0100

    Solved persistent data

---
+++ app/src/main/java/com/example/fooding/EditActivity.java
@@ -0,0 +1,203 @@
+package com.example.fooding;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class EditActivity extends AppCompatActivity {
+
+    private static final int CAM_REQ = 10;
+    private static final int GALLERY_REQ = 11;
+    private static final int STORAGE_PERMISSION_CODE = 100;
+    public static final String URI_PREFS = "uri_prefs";
+    public static final String NAME_PREFS = "name_prefs";
+    public static final String ADDR_PREFS = "address_prefs";
+
+    private CircleImageView avatar;
+    private Button save_btn;
+    private EditText name_et;
+    private EditText addr_et;
+    private Uri selectedImage;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferences.edit();
+        avatar = findViewById(R.id.avatar);
+        save_btn = findViewById(R.id.avatar_btn);
+        name_et = findViewById(R.id.name_et);
+        addr_et = findViewById(R.id.address_et);
+
+        if(preferences.contains(EditActivity.URI_PREFS))
+            avatar.setImageURI(Uri.parse(preferences.getString(URI_PREFS, "")));
+        if(preferences.contains(EditActivity.NAME_PREFS))
+            name_et.setText(preferences.getString(NAME_PREFS, ""));
+        if(preferences.contains(EditActivity.ADDR_PREFS))
+            addr_et.setText(preferences.getString(ADDR_PREFS, ""));
+
+        avatar.setOnClickListener(e -> {
+            if(isStoragePermissionGranted()) {
+                selectImage();
+            }
+        });
+
+        save_btn.setOnClickListener(e -> {
+            if(!(name_et.getText().toString().equals(preferences.getString(NAME_PREFS, "")))) {
+                editor.putString(NAME_PREFS, name_et.getText().toString());
+                editor.apply();
+            }
+            if(!(addr_et.getText().toString().equals(preferences.getString(ADDR_PREFS, "")))) {
+                editor.putString(ADDR_PREFS, addr_et.getText().toString());
+                editor.apply();
+            }
+            if(selectedImage != null && !(selectedImage.toString().equals(preferences.getString(URI_PREFS, "")))) {
+                editor.putString(URI_PREFS, selectedImage.toString());
+                editor.apply();
+            }
+        });
+    }
+
+    public boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission())
+                return true;
+            else
+                requestStoragePermission();
+        }
+        else {
+            return true;
+        }
+        return false;
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, (dialog, item) -> {
+            if (items[item].equals("Take Photo")) {
+                cameraIntent();
+            } else if (items[item].equals("Choose from Library")) {
+                galleryIntent();
+            } else if (items[item].equals("Cancel")) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    @SuppressLint("IntentReset")
+    private void galleryIntent() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(intent, GALLERY_REQ);
+    }
+
+    private void cameraIntent() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        selectedImage = FileProvider.getUriForFile(EditActivity.this, BuildConfig.APPLICATION_ID + ".provider", Objects.requireNonNull(getOutputMediaFile()));
+        takePicture.putExtra(MediaStore.EXTRA_OUTPUT, selectedImage);
+        takePicture.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivityForResult(takePicture, CAM_REQ);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case CAM_REQ:
+                if (imageReturnedIntent != null && resultCode == RESULT_OK) {
+                    /* Bitmap bm = (Bitmap)imageReturnedIntent.getExtras().get("data");
+                    avatar.setImageBitmap(bm); */
+                    avatar.setImageURI(selectedImage);
+                }
+                break;
+            case GALLERY_REQ:
+                if (imageReturnedIntent != null && resultCode == RESULT_OK) {
+                    selectedImage = imageReturnedIntent.getData();
+                    avatar.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MAD-photo", "Oops! Failed create directory");
+                return null;
+            }
+        }
+
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +"IMG_"+ timeStamp + ".jpg");
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(EditActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed to store images")
+                    .setPositiveButton("Ok", (dialog, which) -> ActivityCompat.requestPermissions(EditActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE))
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case STORAGE_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/fooding/MainActivity.java
+++ app/src/main/java/com/example/fooding/MainActivity.java
@@ -1,67 +1,44 @@
 package com.example.fooding;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AlertDialog;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Objects;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int CAM_REQ = 10;
-    private static final int GALLERY_REQ = 11;
-    private static final int STORAGE_PERMISSION_CODE = 100;
-
-    private CircleImageView image_btn;
-    private Uri file;
+    SharedPreferences preferences;
+    private CircleImageView avatar;
+    private TextView name_tv;
+    private TextView addr_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear);
+        setContentView(R.layout.activity_main);
 
-        image_btn = findViewById(R.id.avatar);
-        image_btn.setOnClickListener(e -> {
-            if(isStoragePermissionGranted()) {
-                selectImage();
-            }
-        });
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        avatar = findViewById(R.id.avatar);
+        name_tv = findViewById(R.id.name_text);
+        addr_tv = findViewById(R.id.address_text);
     }
 
-    public boolean isStoragePermissionGranted() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkPermission())
-                return true;
-            else
-                requestStoragePermission();
-        }
-        else {
-            Log.d("MAD-permission","Permission is granted");
-            return true;
-        }
-        return false;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(preferences.contains(EditActivity.URI_PREFS))
+            avatar.setImageURI(Uri.parse(preferences.getString(EditActivity.URI_PREFS, "")));
+        if(preferences.contains(EditActivity.NAME_PREFS))
+            name_tv.setText(preferences.getString(EditActivity.NAME_PREFS, ""));
+        if(preferences.contains(EditActivity.ADDR_PREFS))
+            addr_tv.setText(preferences.getString(EditActivity.ADDR_PREFS, ""));
     }
 
     @Override
@@ -71,111 +48,12 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private void selectImage() {
-        final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Add Photo!");
-        builder.setItems(items, (dialog, item) -> {
-            if (items[item].equals("Take Photo")) {
-                cameraIntent();
-            } else if (items[item].equals("Choose from Library")) {
-                galleryIntent();
-            } else if (items[item].equals("Cancel")) {
-                dialog.dismiss();
-            }
-        });
-        builder.show();
-    }
-
-    @SuppressLint("IntentReset")
-    private void galleryIntent() {
-        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        getIntent.setType("image/*");
-
-        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        pickIntent.setType("image/*");
-
-        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
-
-        startActivityForResult(chooserIntent, GALLERY_REQ);
-    }
-
-    private void cameraIntent() {
-        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        file = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", Objects.requireNonNull(getOutputMediaFile()));
-        takePicture.putExtra(MediaStore.EXTRA_OUTPUT, file);
-        takePicture.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-        startActivityForResult(takePicture, CAM_REQ);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-
-        switch (requestCode) {
-            case CAM_REQ:
-                if (imageReturnedIntent != null && resultCode == RESULT_OK) {
-                    /* Bitmap bm = (Bitmap)imageReturnedIntent.getExtras().get("data");
-                    image_btn.setImageBitmap(bm); */
-                    image_btn.setImageURI(file);
-                }
-                break;
-            case GALLERY_REQ:
-                if (imageReturnedIntent != null && resultCode == RESULT_OK) {
-
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    image_btn.setImageURI(selectedImage);
-
-                }
-                break;
-        }
-    }
-
-    private static File getOutputMediaFile() {
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "CameraDemo");
-
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.d("MAD-photo", "Oops! Failed create directory");
-                return null;
-            }
-        }
-
-        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +"IMG_"+ timeStamp + ".jpg");
-    }
-
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        return result == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestStoragePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            new AlertDialog.Builder(this)
-                    .setTitle("Permission needed")
-                    .setMessage("This permission is needed to store images")
-                    .setPositiveButton("Ok", (dialog, which) -> ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE))
-                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
-                    .create().show();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-        }
-    }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case STORAGE_PERMISSION_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
-                break;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.edit_mode) {
+            Intent i = new Intent(getApplicationContext(), EditActivity.class);
+            startActivity(i);
         }
+        return true;
     }
 }
