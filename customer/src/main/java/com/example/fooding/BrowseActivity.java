2978b74fda3132c427169d7313c5f0f8bad5b4c8
LasagneArrosto <lasagne.arrosto@gmail.com>
2019-05-08 22:59:23 +0200

    Spinner implemented, can now filter restaurants by type.

--- customer/src/main/java/com/example/fooding/BrowseActivity.java
+++ customer/src/main/java/com/example/fooding/BrowseActivity.java
@@ -1,7 +1,9 @@
 package com.example.fooding;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +11,11 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -39,38 +47,112 @@ public class BrowseActivity extends AppCompatActivity implements BrowseAdapter.I
         getSupportActionBar().setHomeButtonEnabled(true);
 
         recycle = (RecyclerView) findViewById(R.id.restaurant_rView);
-        rLayoutManager = new LinearLayoutManager(this);
+        rLayoutManager = new LinearLayoutManager(BrowseActivity.this);
         recycle.setLayoutManager(rLayoutManager);
+        adapter = new BrowseAdapter(BrowseActivity.this, list);
+        adapter.setClickListener(BrowseActivity.this);
+        recycle.setAdapter(adapter);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
 
-        myRef.child("restaurateur").addListenerForSingleValueEvent(new ValueEventListener() {
+        Spinner spinner = (Spinner) findViewById(R.id.type_et);
+        String[] types = getResources().getStringArray(R.array.food_types);
+        final List<String> typesList = new ArrayList<>(Arrays.asList(types));
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                BrowseActivity.this ,R.layout.support_simple_spinner_dropdown_item, typesList){
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                for(DataSnapshot dataSnapshot1 :dataSnapshot.getChildren()){
-                    Restaurant fire = new Restaurant();
-                    fire.setName(dataSnapshot1.child("name").getValue().toString());
-                    fire.setType(dataSnapshot1.child("type").getValue().toString());
-                    fire.setUid(dataSnapshot1.getKey());
-                    fire.setUri(dataSnapshot1.getKey()+"/photo.jpg");
-                    list.add(fire);
-
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
                 }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerArrayAdapter);
 
-                adapter = new BrowseAdapter(BrowseActivity.this, list);
-                adapter.setClickListener(BrowseActivity.this);
-                recycle.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
 
+                    list.clear();
+                    adapter.notifyDataSetChanged();
+
+                    myRef.child("types").child(selectedItemText).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+                            for(DataSnapshot dataSnapshot1 :dataSnapshot.getChildren()){
+                                Restaurant fire = new Restaurant();
+                                fire.setUid(dataSnapshot1.getKey());
+                                fire.setUri(dataSnapshot1.getKey()+"/photo.jpg");
+                                myRef.child("restaurateur").child(dataSnapshot1.getKey()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        fire.setName(dataSnapshot.child("name").getValue().toString());
+                                        fire.setType(dataSnapshot.child("type").getValue().toString());
+                                        list.add(fire);
+                                        adapter.notifyItemInserted(list.size()-1);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w("Hello", "Failed to read value.", error.toException());
+                        }
+                    });
+                }
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("Hello", "Failed to read value.", error.toException());
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
+
+
+
 
     }
 
